18e799eb3d710e4ad8c3c148a11bdeaf295a0f57
Peterdelavega20 <144301319+Peterdelavega20@users.noreply.github.com>
2023-09-14 11:00:37 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,18 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I'm Peter Don H. Dela Vega, and it's a pleasure to meet you. With a passion for both business and leisure, I've managed to strike a harmonious balance between work and play. When I'm not busy running my own business, you'll likely find me shooting hoops on the basketball court, belting out tunes with my singing voice, or immersed in the virtual world of computer games. Adobo holds a special place in my heart and palate; its savory blend of flavors never fails to satisfy my taste buds. But my love for learning is just as strong as my love for food. I pride myself on being a quick and eager learner, always seeking new opportunities to expand my knowledge and skills.So, whether I'm making strategic business decisions, sinking three-pointers, hitting high notes, or embarking on new learning adventures, I'm dedicated to making the most out of every moment. Feel free to join me on this journey of growth, fun, and exploration!
+        <p>10 Things That Require Zero Talent
+
+1. Being On Time
+2. Making An Effort
+3. Being High Energy
+4. Having A Positive Attitude
+5. Being Passionate
+6. Using Good Body Language
+7. Being Coachable
+8. Doing A Little Extra
+9. Being Prepared
+10. Having A Strong Work Ethic
 </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
